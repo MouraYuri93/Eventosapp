05954e37da36dd107738f2c59f1f7bc7cc779a2d
Yuri Moura <mourayuri93@gmail.com>
2023-05-19 16:48:54 -0300

    Realizando modificações

--- src/main/java/com/eventosapp/repository/EventoRepository.java
+++ src/main/java/com/eventosapp/repository/EventoRepository.java
@@ -4,5 +4,6 @@ import com.eventosapp.models.Evento;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EventoRepository extends CrudRepository<Evento, String> {
+    Evento findByCodigo(long codigo);
 
 }
