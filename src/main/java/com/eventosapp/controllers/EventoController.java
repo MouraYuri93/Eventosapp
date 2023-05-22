23ce91e07a0db0966fa1dc2d4c4ddc5d5c37cac1
Yuri Moura <mourayuri93@gmail.com>
2023-05-21 22:51:03 -0300

    Atualizando projeto

--- src/main/java/com/eventosapp/controllers/EventoController.java
+++ src/main/java/com/eventosapp/controllers/EventoController.java
@@ -4,28 +4,39 @@ import com.eventosapp.models.Evento;
 import com.eventosapp.repository.EventoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 public class EventoController {
 
     @Autowired
     private EventoRepository er;
-    @RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
-    public String form() {
+    @RequestMapping(value="/cadastrarEvento", method=RequestMethod.GET)
+    public String form(){
         return "evento/formEvento";
     }
-    @RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
-    public String form(Evento evento) {
+
+    @RequestMapping(value="/cadastrarEvento", method=RequestMethod.POST)
+    public String form(@Valid Evento evento, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            attributes.addFlashAttribute("mensagem", "Verifique os campos!");
+            return "redirect:/cadastrarEvento";
+        }
+
         er.save(evento);
+        attributes.addFlashAttribute("mensagem", "Evento cadastrado com sucesso!");
         return "redirect:/cadastrarEvento";
     }
     @RequestMapping("/eventos")
     public ModelAndView listaEventos(){
-        ModelAndView mv = new ModelAndView("index");
+        ModelAndView mv = new ModelAndView("listaEventos");
         Iterable<Evento> eventos = er.findAll();
         mv.addObject("eventos", eventos);
         return mv;
