dca5d8aa9d64a7259a5f20fb99bf3bccb38eee53
Yuri Moura <mourayuri93@gmail.com>
2023-05-28 01:42:03 -0300

    Atualizando projeto

--- src/main/java/com/eventosapp/models/Evento.java
+++ src/main/java/com/eventosapp/models/Evento.java
@@ -1,12 +1,15 @@
 package com.eventosapp.models;
 
 import jakarta.persistence.*;
+
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 public class Evento implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
