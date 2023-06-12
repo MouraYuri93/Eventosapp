9abd4419af052b99d272a37bd08e36a3ddcf2d8f
Yuri Moura <mourayuri93@gmail.com>
2023-06-12 02:14:25 -0300

    Atualizando...

--- src/main/resources/static/materialize/js/script.js
+++ src/main/resources/static/materialize/js/script.js
@@ -9,3 +9,22 @@
 
   // Configura um temporizador para chamar a função hideFlashMessage após o tempo especificado
   setTimeout(hideFlashMessage, hideTimeout);
+
+  // Função para formatar a data no formato dd/mm/aaaa
+  function formatarData(data) {
+    var partes = data.split('-');
+    var dia = partes[2];
+    var mes = partes[1];
+    var ano = partes[0];
+    return dia + '/' + mes + '/' + ano;
+  }
+
+  // Obter todas as células de data na tabela de eventos
+  var celulasData = document.querySelectorAll('.listaEventos tbody td:nth-child(3)');
+
+  // Iterar sobre as células de data e formatar o conteúdo
+  for (var i = 0; i < celulasData.length; i++) {
+    var data = celulasData[i].textContent;
+    var dataFormatada = formatarData(data);
+    celulasData[i].textContent = dataFormatada;
+  }
